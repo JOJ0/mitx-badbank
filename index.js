c7a9ec851f614872fc3c640a47029ad9d4532e46
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-04 09:31:35 +0100

    Add createContext, wrap existing routes in jsx obj
    auto formatting enabled before this commit in vscode, which moved some
    unrelated things.

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+const UserContext = React.createContext(null);
+
 function Spa() {
-  const Route       = ReactRouterDOM.Route;
-  const Link        = ReactRouterDOM.Link;
-  const HashRouter  = ReactRouterDOM.HashRouter;
+  const Route = ReactRouterDOM.Route;
+  const Link = ReactRouterDOM.Link;
+  const HashRouter = ReactRouterDOM.HashRouter;
 
   return (
     <HashRouter>
       <div>
-          <h1>Routing - Hello World</h1>
-          <Link to="/">Home</Link> -- 
-          <Link to="/about/">About</Link> -- 
-          <Link to="/products">Products</Link>
-          <hr/>
-          <Route path="/" exact    component={Home}     />
-          <Route path="/about/"    component={About}    />          
+        <h1>Routing - Hello World</h1>
+        <Link to="/">Home</Link> --
+        <Link to="/about/">About</Link> --
+        <Link to="/products">Products</Link>
+        <hr />
+        <UserContext.Provider values={{ users: "peter" }}>
+          <Route path="/" exact component={Home} />
+          <Route path="/about/" component={About} />
           <Route path="/products/" component={Products} />
+        </UserContext.Provider>
       </div>
     </HashRouter>
   );
 }
 
-ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Spa />, document.getElementById("root"));

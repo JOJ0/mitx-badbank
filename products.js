7b215d2177c5101c8c403123e60f9b5cc7f3a74e
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-04 10:17:31 +0100

    Random user gen and add to ctx on products page

--- products.js
+++ products.js
@@ -1,8 +1,14 @@
-function Products(){
-    return (
-        <div>
-            <h3>Products Component</h3>
-            <p>List of the the product we make</p>
-        </div>
-    );
+function Products() {
+  const ctx = React.useContext(UserContext);
+  ctx.users.push(Math.random().toString(36).substr(2, 5));
+
+  return (
+    <div>
+      <h3>Products Component</h3>
+      <p>
+        List of the the product we make
+        {JSON.stringify(ctx.users)}
+      </p>
+    </div>
+  );
 }

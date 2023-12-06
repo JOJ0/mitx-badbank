5e754cf78675a0a0c500cb7cd994332dfc04f971
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-06 15:30:35 +0100

    Use Card comp in home.js

--- home.js
+++ home.js
@@ -1,13 +1,12 @@
 function Home() {
-  const ctx = React.useContext(UserContext);
 
   return (
-    <div>
-      <h3>Home Component</h3>
-      <p>
-        Welcome to the site - happy to see you
-        {JSON.stringify(ctx.users)}
-      </p>
-    </div>
+    <Card
+      txtcolor="black"
+      header="BadBank Landing Page"
+      title="Welcome to the bank"
+      text="You can use this bank"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
+    />
   );
 }

1b529dd5d2fffd3675f4ed6c5958533d719d8601
J0J0 Todos <jojo@peek-a-boo.at>
2023-12-06 10:46:07 +0100

    Add Nav component based on Route and HashRouter

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const UserContext = React.createContext(null);
+const Route = ReactRouterDOM.Route;
+const HashRouter = ReactRouterDOM.HashRouter;
+const Link = ReactRouterDOM.Link;
 
 function Spa() {
-  const Route = ReactRouterDOM.Route;
-  const Link = ReactRouterDOM.Link;
-  const HashRouter = ReactRouterDOM.HashRouter;
 
   return (
     <HashRouter>
       <div>
         <h1>Routing - Hello World</h1>
-        <Link to="/">Home</Link> --
-        <Link to="/about/">About</Link> --
-        <Link to="/products">Products</Link>
+        <Nav />
         <hr />
         <UserContext.Provider value={{ users: ["peter"] }}>
           <Route path="/" exact component={Home} />

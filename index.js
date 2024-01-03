9a67e6e67c4a964298edbf60f21eb663cdc9e3d5
J0J0 Todos <jojo@peek-a-boo.at>
2024-01-03 11:34:02 +0100

    Remove pages Login and Balance

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ function Spa() {
 		}>
           <Route path="/" exact component={Home} />
           <Route path="/createaccount/" exact component={CreateAccount} />
-          <Route path="/login/" exact component={Login} />
           <Route path="/deposit/" exact component={Deposit} />
           <Route path="/withdraw/" exact component={Withdraw} />
-          <Route path="/balance/" exact component={Balance} />
           <Route path="/alldata/" exact component={AllData} />
         </UserContext.Provider>
       </div>
--- navbar.js
+++ navbar.js
@@ -21,16 +21,10 @@ function NavBar() {
               <Link className="nav-link" to="/createaccount/">Create Account</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/login/">Login</Link>
-            </li>
-            <li className="nav-item">
               <Link className="nav-link" to="/deposit/">Deposit</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/withdraw/">Withdraw</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/balance/">Balance</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/alldata/">All Data</Link>

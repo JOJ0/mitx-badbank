41a68821e0900c60a3e2135b093c8585c990622b
J0J0 Todos <jojo@peek-a-boo.at>
2024-01-05 12:31:51 +0100

    Implement disabling submit btn requirement
    - Refactor name input field to use a handleChange function, instead of
      directly calling setName when the onChange event is happening.
    - handleChange() sets the submitAllowed state to false if it receives a
      non-empty value.
    - The className of the submit button is set via styleSubmitButton()
      which respects the submitAllowed state variable.

--- createaccount.js
+++ createaccount.js
@@ -4,6 +4,7 @@ function CreateAccount(){
   const [name, setName]         = React.useState('');
   const [email, setEmail]       = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [submitAllowed, setSubmitAllowed] = React.useState(false);
   const ctx = React.useContext(UserContext);
 
   function validate(field, label) {
@@ -34,6 +35,31 @@ function CreateAccount(){
     setPassword('');
     setStatus('');
     setShow(true);
+    setSubmitAllowed(false);
+  }
+
+  function handleChange(event) {
+    console.log(`handleChange received ${event.target.value} from field ${event.target.id}`);
+    // console.log(event)  // prints all data of the event object, use for detailed debugging
+    let fieldName = event.target.id;
+    let fieldValue = event.target.value;
+    // We have to update the state variable since the input field's "value"
+    // property uses it (otherwise it would stay empty)
+    if (fieldName == 'name') setName(fieldValue);
+    // No matter which field, if it's empty, disable submit button; sufficient for now...
+    if (fieldValue == '') {
+      setSubmitAllowed(false);
+    }
+    else {
+      setSubmitAllowed(true);
+    }
+  };
+
+  function styleSubmitButton() {
+    let classes = "btn btn-light";
+    if (! submitAllowed) classes += " disabled";
+    console.log("In styleSubmitButton classes is: " + classes)
+    return classes
   }
 
   return (
@@ -44,13 +70,13 @@ function CreateAccount(){
       body={show ? (
         <>
         Name<br/>
-        <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+        <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={handleChange} /><br/>
         Email address<br/>
         <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
         Password<br/>
         <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
 
-        <button type="submit" className="btn btn-light" onClick={handleCreate}>Create account</button>
+        <button type="submit" className={styleSubmitButton()} onClick={handleCreate}>Create account</button>
         </>
       ):(
         <>

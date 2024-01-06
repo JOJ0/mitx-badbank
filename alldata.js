c7d57abad348abecb89af6aa7769722b19d7724b
J0J0 Todos <jojo@peek-a-boo.at>
2024-01-06 14:39:17 +0100

    Implement all data page
    Dear MITx course graders, I'm not sure what "All user submissions appear
    on "All Data' page" means exactly. I'm  displaying all data of the
    context users array here. Hope that's sufficient.

--- alldata.js
+++ alldata.js
@@ -1,12 +1,45 @@
 function AllData() {
   const ctx = React.useContext(UserContext);
+  const Table = ReactBootstrap.Table;
+
+  const TableBody = () => {
+    const rows = ctx.users.map((row, index) => {
+      return (
+        <tr key={index}>
+          <th scope="row">{index + 1}</th>
+          <td>{row.name}</td>
+          <td>{row.email}</td>
+          <td>redacted</td>
+          <td>{row.balance}</td>
+        </tr>
+      );
+    });
+    return rows;
+  };
 
   return (
     <Card
       header="All Data"
       title="Your personal termination overview."
-      text={"User: " + JSON.stringify(ctx.users[0].name)}
-      body={JSON.stringify(ctx.users[0].name)}
+      text={"Money loss guaranteed!"}
+      body={
+        <>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableBody />
+          </tbody>
+        </Table>
+        </>
+      }
       txtcolor="black"
     />
   );

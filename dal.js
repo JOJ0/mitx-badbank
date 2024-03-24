8890392e57e4d2861f1acac8c48ca6c98840d1d3
J0J0 Todos <jojo@peek-a-boo.at>
2024-03-24 15:11:35 +0100

    Add data abstr layer accessing db
    and adapt index.js api endpoints to return data.
    
    Only finished for now is /account/all endpoint.

---
+++ dal.js
@@ -0,0 +1,82 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// FIXME, not sure if these timeout strings actually work
+const uri = 'mongodb://localhost:27017?connectTimeoutMS=1000&socketTimeoutMS=1000';  // ?directConnection=true
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
+var db = null;  // instantiate to something
+console.log("We are running Data Abstraction Layer.")
+
+// Test connection and get db handle
+async function connect() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    var reply = await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB! Reply was:");
+    console.log(reply);
+    // Instantiate a db object
+    db = client.db('myproject');
+    return db;
+  }
+  catch(err) {
+    console.log(`We caught an error in our main function run(): ${err}.`);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
+// Create user account
+async function create(name, email, password){
+    await client.connect();
+    db = client.db('myproject');
+    return new Promise((resolve, reject) => {    
+        // Collect the data we want to add into an object
+        const newData = {name, email, password, balance: 0};
+        // And insert into the 'users' collection
+        db.collection('users').insertOne(newData, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
+}
+
+// Return all users
+async function all_old(){
+    await client.connect();
+    db = client.db('myproject');
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({})  // No criteria == all
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+async function all(){
+  await client.connect();
+  db = client.db('myproject');
+  var collection = db.collection('users');
+  try {
+    res = collection.find({}).toArray();
+    console.log(`dal.all() returning data ${res}`);
+    return(res);
+  }
+  catch (err) {
+    console.error(`Something went wrong: ${err}`);
+    return(err);
+  }
+}
+
+module.exports = {create, all};
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var dal = require('./dal.js');
 
 app.use(express.static('public'));
 app.use(cors());
 
 // Create user account route
 app.get('/account/create/:name/:email/:password', (req, res) => {
-  res.send({
-    name: req.params.name,
-    email: req.params.email,
-    password: req.params.password
-  })
+  dal.create(req.params.name, req.params.email, req.params.password)  
+    .then((newUser) => {
+      msg = `Created new user: ${newUser}`
+      console.log(msg);
+      res.send(msg);
+    });
 })
 
 
 // List all accounts route
 app.get('/account/all', (req, res) => {
-  res.send({
-    name: 'Peter',
-    email: 'peter@mit.edu',
-    password: 'secret'
-  })
+  dal.all()
+    .then((documents) => {
+      console.log('/account/all endpoint returning data.');
+      res.send(documents);
+    })
 })
 
 var port = 3000;

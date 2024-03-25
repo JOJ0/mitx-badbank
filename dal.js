37ad5650b5cea1fa9fab1f57d368c0b6631169c6
J0J0 Todos <jojo@peek-a-boo.at>
2024-03-25 11:12:55 +0100

    Experiment with separate mongo connect file dal_connect.js

--- dal.js
+++ dal.js
@@ -1,40 +1,6 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-// FIXME, not sure if these timeout strings actually work
-const uri = 'mongodb://localhost:27017?connectTimeoutMS=1000&socketTimeoutMS=1000';  // ?directConnection=true
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  }
-);
-var db = null;  // instantiate to something
+const db = require("./dal_connect.js");
 console.log("We are running Data Abstraction Layer.")
-
-// Test connection and get db handle
-async function connect() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    var reply = await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB! Reply was:");
-    console.log(reply);
-    // Instantiate a db object
-    db = client.db('myproject');
-    return db;
-  }
-  catch(err) {
-    console.log(`We caught an error in our main function run(): ${err}.`);
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
+console.log(db)
 
 // Create user account
 async function create(name, email, password){
@@ -61,9 +27,9 @@ async function all_old(){
 }
 
 async function all(){
-  await client.connect();
-  db = client.db('myproject');
-  var collection = db.collection('users');
+  // await client.connect();
+  // db = client.db('myproject');
+  var collection = await db.collection('users');
   try {
     res = collection.find({}).toArray();
     console.log(`dal.all() returning data ${res}`);

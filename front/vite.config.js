5a71c781cb87eed18bc6cb14f292c7708ad4dbcf
J0J0 Todos <jojo@peek-a-boo.at>
2024-04-07 15:49:38 +0200

    Finally a working vite proxy config

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,12 +5,35 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "./portfolio"
   plugins: [react()],
-    resolve: {
+  resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
       '~assets': path.resolve(__dirname, 'src/assets'),
     },
-    // base: "./portfolio"
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace('/api', ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+    },
+  },
+  /*
+  */
 })

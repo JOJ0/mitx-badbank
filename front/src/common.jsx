bfa897b43baf77eaa56e8c1d37f4175fbdb5a2cf
J0J0 Todos <jojo@peek-a-boo.at>
2024-04-09 10:55:52 +0200

    Fix unused React import to only createContext in common.jsx

--- front/src/common.jsx
+++ front/src/common.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import { createContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 // exporting commonly used hooks
-export const UserContext = React.createContext(null);
+export const UserContext = createContext(null);
 
 
 export async function apiPutRequest(url, data) {

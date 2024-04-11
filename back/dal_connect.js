9affa34df21f3d9ca2edcd21de30adc93db0eea4
J0J0 Todos <jojo@peek-a-boo.at>
2024-04-11 11:22:05 +0200

    Add a dotenv debug message to server

--- back/dal_connect.js
+++ back/dal_connect.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import 'dotenv/config';
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}+@termbank.lp2lbop.mongodb.net/?retryWrites=true&w=majority&appName=termbank`
+console.log("Debug dotenv on Heroku:", process.env)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {

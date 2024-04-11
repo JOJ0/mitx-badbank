5aaea3aee184be959ef5c8251a3edca853d4542b
J0J0 Todos <jojo@peek-a-boo.at>
2024-04-11 11:32:54 +0200

    Refine dal_connect.js and cleanup crap

--- back/dal_connect.js
+++ back/dal_connect.js
@@ -16,33 +16,13 @@ const client = new MongoClient(uri, {
 let conn;
 try {
   conn = await client.connect();
+  var reply = await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB! Reply was:");
+  console.log(reply);
 } catch(e) {
   console.error(e);
 }
 let db = conn.db("myproject");
 
-// FIXME
-// Test connection and get db handle
-async function connect() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    var reply = await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB! Reply was:");
-    console.log(reply);
-    // Instantiate a db object
-    db = client.db('myproject');
-    return db;
-  }
-  catch(err) {
-    console.log(`We caught an error in our main function run(): ${err}.`);
-  }
-  // finally {
-  //   // Ensures that the client will close when you finish/error
-  //   await client.close();
-  // }
-}
-// FIXME
 
 export default db;
